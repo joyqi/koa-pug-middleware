493c5391439f349dfc5633dcd3896273af4effa7
joyqi <magike.net@gmail.com>
2022-11-25 13:53:22 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # koa-pug-middleware
+
+This is a Koa middleware wrapper for [pug](https://pugjs.org/) template engine.
+
+## Installation
+
+```bash
+npm install -s koa-pug-middleware
+```
+
+## Usage
+
+```typescript
+import * as Koa from 'koa';
+import * as pug from 'koa-pug-middleware';
+
+const app = new Koa();
+
+app.use(pug({
+  debug: false,
+  pretty: false,
+  compileDebug: false,
+  locals: {
+    globalFoo: 'bar'  // You can set default locals here
+  },
+  basedir: './views'
+}));
+
+app.use(async (ctx) => {
+  ctx.setLocal('foo', 'bar');  // You can set locals here
+});
+
+app.use(async (ctx) => {
+  await ctx.render('index', {
+    foo: 'bar'  // You can set locals here
+  });
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-// TODO: export your library
+import { Options, LocalsObject, compileFile, compileTemplate } from 'pug';
+import { Middleware } from 'koa';
+
+declare module 'koa' {
+    interface Context {
+        render: (view: string, locals?: LocalsObject, options?: RenderOptions) => Promise<void>;
+        setLocal: (key: string, value: any) => void;
+    }
+}
+
+type DefaultOptions = Omit<Options, 'filename'> & { locals?: LocalsObject };
+type RenderOptions = Omit<DefaultOptions, 'locals'>;
+
+export default function pug(defaultOptions: DefaultOptions): Middleware {
+    const views = new Map<string, compileTemplate>();
+
+    // Compile the template and cache it
+    // We use custom cache because pug's cache is not exposed
+    function getView(view: string, options?: RenderOptions) {
+        const filename = view.endsWith('.pug') ? view : `${view}.pug`;
+        const reanderOptions = { ...defaultOptions, ...options, filename};
+
+        if (!views.has(filename)) {
+            const cache = false;
+            const fn = compileFile(filename, { ...reanderOptions, cache });
+
+            if (!reanderOptions.cache) {
+                return fn;
+            }
+
+            views.set(filename, fn);
+        }
+
+        return views.get(filename) as compileTemplate;
+    }
+
+    return async (ctx, next) => {
+        const defaultLocals = defaultOptions.locals || {};
+
+        // Set custom local variables
+        ctx.setLocal = (key: string, value: any) => {
+            defaultLocals[key] = value;
+        };
+
+        // Add render method to context
+        ctx.render = async (view: string, locals?: LocalsObject, options?: RenderOptions) => {
+            const fn = getView(view, options);
+            ctx.body = fn({ ...defaultLocals, ...locals });
+        };
+
+        await next();
+    };
+}
